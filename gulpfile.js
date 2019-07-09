f57bc5e777f3316d7a64fbe5bd56bc654cf91d52
LeloushLamberg <egor.bil.1987@yandex.ru>
2019-07-09 10:22:44 +0800

    final assembly

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css')
 const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const tinyPNG = require('gulp-tinypng-compress');
 
-gulp.task('default', defaultTask)
-
-function defaultTask(done){
-  
-  done();
-}
 
 gulp.task('minify-css', function(done){
     gulp.src('./src/css/*.css')
@@ -23,3 +19,28 @@ gulp.task('minify-js', function(done){
     .pipe(gulp.dest('dist/js/'))
   done()
 });
+gulp.task('minify-html', function(done){
+    gulp.src('./src/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist/'))
+  done()
+});
+gulp.task('move-fonts', function(done){
+  gulp.src('./src/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts/'))
+done()
+});
+gulp.task('tinypng', function (done) {
+  gulp.src('./src/img/*.{png,jpg,jpeg}')
+      .pipe(tinyPNG({
+          key: 'D47E7DbIJxCb8zKUnafAgiZKFBjOXMC4'
+      }))
+      .pipe(gulp.dest('dist/imag/'));
+      done()
+});
+gulp.task('default', gulp.series('minify-css','minify-js', 'minify-html', 'move-fonts', 'tinypng', function (done){
+  
+  done();
+}));
